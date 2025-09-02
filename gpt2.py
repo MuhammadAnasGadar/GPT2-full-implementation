c832db7d84050e29ec301d95755559060e933818
MuhammadAnasGadar <muhammad.anas@talabat.com>
2025-09-03 01:09:37 +0400

    fix: shared token embedding not position embedding

--- gpt2.py
+++ gpt2.py
@@ -106,7 +106,7 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         # weight sharing scheme
-        self.transformer.wpe.weight = self.lm_head.weight
+        self.transformer.wte.weight = self.lm_head.weight
 
         # init params
         self.apply(self._init_weights)
